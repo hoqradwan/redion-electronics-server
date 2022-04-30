fec34bb92b14bf25af5b5b86521b0740a242d675
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-04-30 20:09:02 +0600

    db connected

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4b1x8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async() => {
+    client.connect()
+    console.log('connected')
+        const productCollection = client.db("redionElectronics").collection("products");
+        // perform actions on the collection object
+      
+}
+run();
+
+
+app.get('/', (req, res)=>{
+    res.send('Hey bro, How are you!!')
+})
+app.listen(port, ()=>{
+    console.log('server is running')
+})
