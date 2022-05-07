23f282461c4a2a6e1e1d2fb4fdac6c0fee8a120b
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-05-07 14:21:39 +0600

    one get api removed

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ const run = async () => {
       res.send(products);
     });
 
-    app.get("/products/:id", async (req, res) => {
+  /*   app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
       const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
       res.send(product);
-    });
+    }); */
     app.get("/products/:id", async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.query.email;
